a76e989bb66190da0dbb1bb7275ec57193558f70
Samy Hamdy <samyhamdy221152@gmail.com>
2025-07-20 11:01:57 +0300

    add assets

---
+++ lib/core/app_assets/app_assets.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for animationsOnBoardingAnimation1
+  /// assets/animations/onBoardingAnimation1.json
+  static const String animationsOnBoardingAnimation1 = "assets/animations/onBoardingAnimation1.json";
+
+  /// Assets for animationsOnBoardingAnimation2
+  /// assets/animations/onBoardingAnimation2.json
+  static const String animationsOnBoardingAnimation2 = "assets/animations/onBoardingAnimation2.json";
+
+  /// Assets for animationsOnBoardingAnimation3
+  /// assets/animations/onBoardingAnimation3.json
+  static const String animationsOnBoardingAnimation3 = "assets/animations/onBoardingAnimation3.json";
+
+  /// Assets for fontsGESSTwoBold
+  /// assets/fonts/GE_SS_Two_Bold.otf
+  static const String fontsGESSTwoBold = "assets/fonts/GE_SS_Two_Bold.otf";
+
+  /// Assets for fontsGESSTwoLight
+  /// assets/fonts/GE_SS_Two_Light.otf
+  static const String fontsGESSTwoLight = "assets/fonts/GE_SS_Two_Light.otf";
+
+  /// Assets for fontsGESSTwoMedium
+  /// assets/fonts/GE_SS_Two_Medium.otf
+  static const String fontsGESSTwoMedium = "assets/fonts/GE_SS_Two_Medium.otf";
+
+  /// Assets for fontsStc
+  /// assets/fonts/stc.otf
+  static const String fontsStc = "assets/fonts/stc.otf";
+
+  /// Assets for imagesHerohome
+  /// assets/images/herohome.png
+  static const String imagesHerohome = "assets/images/herohome.png";
+
+  /// Assets for imagesQrcode
+  /// assets/images/qrcode.png
+  static const String imagesQrcode = "assets/images/qrcode.png";
+
+  /// Assets for logoLogo
+  /// assets/logo/logo.png
+  static const String logoLogo = "assets/logo/logo.png";
+
+  /// Assets for logoSplashLogo
+  /// assets/logo/splash_logo.png
+  static const String logoSplashLogo = "assets/logo/splash_logo.png";
+}
+
